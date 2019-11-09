0511076072ec0b80fb975f4e8577cb137eff9265
Sascha Adams <Sascha@Adams1.de>
2019-11-09 15:47:33 +0100

    getboard and setboard implemented static ressources implemented

--- src/com/company/GameOfLife.java
+++ src/com/company/GameOfLife.java
@@ -8,12 +8,14 @@ public class GameOfLife {
     private byte[][] _calcBoard;
     private int _size;
     private int _step;
+    private int _startingPopulation;
 
     public GameOfLife(int startingSize, int startingPopulation){
         if(startingPopulation > (startingSize*startingSize*0.9)){
             double sPop = (startingSize * startingSize * 0.9);
             startingPopulation = (int) sPop;
         }
+        _startingPopulation = startingPopulation;
         _size = startingSize;
         startingSize = startingSize + 2;
         _board = new byte[startingSize][startingSize];
@@ -59,17 +61,26 @@ public class GameOfLife {
         }
     }
 
+    public void Clear(){
+        ClearBoard(_board);
+    }
+
+    public void SetPoint(int r, int c){
+        _board[r][c] = 1;
+    }
+
     public String GetBoardAsHTML(){
         String result = "";
         result = "";
-        result += "<table border=1 spacing=\"0\" padding=\"0\" style=\"border: 1px solid black; border-collapse: collapse;\">";
+        result += "<table>";
         for (int r = 1; r <= _size; r++) {
             result += "<tr>";
             for (int c = 1; c <= _size; c++) {
+                String content = "<a href=\"/?action=set&r="+r+"&c="+c+"\">&nbsp;</a>";
                 if (_board[r][c] == 1){
-                    result += "<td style=\"background-color:green;\"width=16>&nbsp;</td>";
+                    result += "<td class=\"cell1\">"+content+"</td>";
                 } else {
-                    result += "<td width=16>&nbsp;</td>";
+                    result += "<td class=\"cell0\">"+content+"</td>";
                 }
             }
             result += "</tr>";
@@ -78,12 +89,77 @@ public class GameOfLife {
         return result;
     }
 
+    public String GetBoardString(){
+        String result = "";
+        for (int r = 1; r <= _size; r++) {
+            for (int c = 1; c <= _size; c++) {
+                result += _board[r][c];
+            }
+        }
+        return result;
+    }
+
+    public String GetBoardHexString(){
+        String result = "";
+        int pos = 0;
+        int act = 0;
+        for (int r = 1; r <= _size; r++) {
+            for (int c = 1; c <= _size; c++) {
+                act = act + _board[r][c];
+                act = act << 1;
+                pos++;
+                if (pos == 8) {
+                    pos = 0;
+                    result += GetHexFromByte((byte) act);
+                    act = 0;
+                }
+            }
+        }
+        if (pos > 0) result += GetHexFromByte((byte) act);
+        return result;
+    }
+
+    static final String HEXES = "0123456789ABCDEF";
+    public static String GetHexFromByte( byte raw ) {
+        String result = "";
+         result += HEXES.charAt((raw & 0xF0) >> 4);
+         result += HEXES.charAt((raw & 0x0F));
+        return result;
+    }
+
+    public void SetBoardString(String boardString){
+        int pos = 0;
+        for (int r = 1; r <= _size; r++) {
+            for (int c = 1; c <= _size; c++) {
+                String point = "";
+                if (pos < boardString.length()) {
+                    point = boardString.substring(pos, pos+1);
+                }
+                switch (point) {
+                    case "1":
+                        _board[r][c] = 1;
+                        break;
+                    default:
+                        _board[r][c] = 0;
+                        break;
+                }
+
+                pos++;
+            }
+        }
+    }
+
     private void ClearBoard(byte[][] board){
-        for (int r = 0; r < _size; r++) {
-            for (int c = 0; c < _size; c++) {
+        for (int r = 0; r < _size+2; r++) {
+            for (int c = 0; c < _size+2; c++) {
                 board[r][c] = 0;
             }
         }
+    }
+
+    public void Restart(){
+        ClearBoard(_board);
+        InitializeBoard(_board, _startingPopulation);
     }
 
     private void InitializeBoard(byte[][] board, int startingPopulation){
--- src/com/company/RootHandler.java
+++ src/com/company/RootHandler.java
@@ -1,33 +1,172 @@
 package com.company;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RootHandler implements HttpHandler {
 
-    GameOfLife gol;
+    private GameOfLife gol;
+    private Map<String, String> params;
+    private boolean AutoStep;
 
     @Override
     public void handle(HttpExchange he) throws IOException {
+        String queryString = he.getRequestURI().getQuery();
+        String urlPath = he.getRequestURI().getPath();
+        if (!"/".equals(urlPath) && "/".equals(urlPath.substring(0,1))){
+            serveStaticRessource(he, urlPath);
+            return;
+        }
+
+
+        params = parseQueryString(queryString);
+        String boardOutput = "";
+        String boardOutputHex = "";
+
+        String action = getQueryString("action");
+        switch (action){
+            case "autoon":
+                AutoStep = true;
+                break;
+            case "autooff":
+                AutoStep = false;
+                break;
+            case "restart":
+                gol.Restart();
+                break;
+            case "clear":
+                gol.Clear();
+                break;
+            case "set":
+                int row = getQueryInt("r");
+                int column = getQueryInt("c");
+                gol.SetPoint(Integer.parseInt(params.get("r")),Integer.parseInt(params.get("c")));
+                break;
+            case "getboard":
+                boardOutput = gol.GetBoardString();
+                boardOutputHex = "<br>"+gol.GetBoardHexString();
+                break;
+            case "setboard":
+                String boardString = getQueryString("board");
+                if (boardString.length() > 0) gol.SetBoardString(boardString);
+                break;
+            default:
+                gol.Grow();
+                break;
+        }
+
         String response =
             "<html>"+
-            "<head>"+
-            "<meta http-equiv=\"refresh\" content=\"1\" />"+
-            "</head>"+
-            "<h4>Small Game of Life Example in Java</h4>";
-        gol.Grow();
+            "<head>";
+        if(AutoStep) {
+//            response += "<meta http-equiv=\"refresh\" content=\"1 url=/\" />";
+        }
+        response += "<link rel=\"stylesheet\" type=\"text/css\" href=\"/src/my.css\">";
+        response += "</head><body><center>"+
+            "<h4>Small Game of Life Example in Java</h4>"+
+            "<div>"+
+            "<a href=\"?action=restart\" class=\"button\"><button>restart</button></a>&nbsp;"+
+            "<a href=\"?action=clear\" class=\"button\"><button>clear</button></a>&nbsp;"+
+            "<a href=\"?action=getboard\" class=\"button\"><button>get board</button></a>&nbsp;"+
+            "<a href=\"?action=step\" class=\"button\"><button>step</button></a>&nbsp;"+
+            "<a href=\"?action=autoon\" class=\"button\"><button>autostep on</button></a>&nbsp;"+
+            "<a href=\"?action=autooff\" class=\"button\"><button>autostep off</button></a>&nbsp;"+
+            "</div>";
+
+
         response += gol.GetBoardAsHTML();
+        response += "</center>";
+        response += "<div style=\"width: 90%;margin: auto;word-wrap: break-word;\">"+boardOutput+"</div>";
+        response += "<div style=\"width: 90%;margin: auto;word-wrap: break-word;\">"+boardOutputHex+"</div>";
+        if(AutoStep){
+            response += "<script>setTimeout('window.location.href=\"?action=none\";', 10);</script>";
+        }
         response += "</body></html>";
+
         he.sendResponseHeaders(200, response.length());
         OutputStream os = he.getResponseBody();
         os.write(response.getBytes());
         os.close();
     }
 
+    private int getQueryInt(String key) {
+        String value = getQueryString(key);
+        int result = Integer.parseInt(value);
+        return result;
+    }
+
+    private String getQueryString(String key){
+        String result = params.get(key);
+        if (result == null) result = "";
+        return result;
+    }
+
+    private static Map<String, String> parseQueryString(String qs) {
+        Map<String, String> result = new HashMap<>();
+        if (qs == null)
+            return result;
+
+        int last = 0, next, l = qs.length();
+        while (last < l) {
+            next = qs.indexOf('&', last);
+            if (next == -1)
+                next = l;
+
+            if (next > last) {
+                int eqPos = qs.indexOf('=', last);
+                try {
+                    if (eqPos < 0 || eqPos > next)
+                        result.put(URLDecoder.decode(qs.substring(last, next), "utf-8"), "");
+                    else
+                        result.put(URLDecoder.decode(qs.substring(last, eqPos), "utf-8"), URLDecoder.decode(qs.substring(eqPos + 1, next), "utf-8"));
+                } catch (UnsupportedEncodingException e) {
+                    throw new RuntimeException(e); // will never happen, utf-8 support is mandatory for java
+                }
+            }
+            last = next + 1;
+        }
+        return result;
+    }
+
+    private void serveStaticRessource(HttpExchange he, String urlPath) throws IOException{
+        // serve static resource
+        urlPath = urlPath.substring(1);
+        File file = new File(urlPath);
+
+        Headers h = he.getResponseHeaders();
+
+        String line;
+        String resp = "";
+
+        try {
+            File newFile = new File(urlPath);
+            System.out.println("file: " + newFile.getName());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(newFile)));
+            while ((line = bufferedReader.readLine()) != null) {
+                resp += line;
+            }
+            bufferedReader.close();
+            h.add("Content-Type", "text/css");
+            he.sendResponseHeaders(200, resp.length());
+        } catch (IOException e) {
+//                e.printStackTrace();
+            System.out.println("file not found: "+urlPath);
+            resp = "file not found";
+            he.sendResponseHeaders(404,resp.length());
+        }
+        OutputStream os = he.getResponseBody();
+        os.write(resp.getBytes());
+        os.close();
+
+    }
+
     public RootHandler(){
-        gol = new GameOfLife(30, 200);
+        gol = new GameOfLife(40, 300);
     }
 }
